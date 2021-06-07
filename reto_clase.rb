4d2810f79189ddc54c7586cb77623cd566aa1559
alicia0408 <alicent.08.02@gmail.com>
2021-06-07 07:33:13 -0400

    desafios hashes clase 07 ruby

---
+++ reto_clase.rb
@@ -0,0 +1,12 @@
+101.times do |e| 
+    if e % 3 == 0 
+        if e % 5 == 0 
+          puts "el numero #{e} es divisible por 3 y 5" 
+        else 
+        puts "el numero #{e} es divisible por 3"
+    end
+end 
+if e % 5 == 0
+    puts "el numero #{e} es divisilbe por 5" 
+end 
+    end 
